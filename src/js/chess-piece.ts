f207863306c78d9fa818215cb76e7e8a13e9f244
shanovskyi-b <shanovskyi.b@gmail.com>
2019-06-15 13:03:15 +0300

    added some basic styling

--- src/js/chess-piece.ts
+++ src/js/chess-piece.ts
@@ -1,11 +1,15 @@
 'use strict';
 
 type piecesColor = 'black' | 'white';
+type pieceType = 'Pawn' | 'Rook' | 'Knight' | 'Bishop' | 'Queen' | 'King';
 
 class ChessPiece {
   readonly element: HTMLElement = document.createElement('div');
+  type: pieceType;
 
-  constructor ({ color } : { color: piecesColor }) {
+  constructor ({ color, type } : { color: piecesColor, type: pieceType }) {
+    this.type = type;
+
     this.element.classList.add('chess-piece', 'chess-piece--' + color);
   }
 
